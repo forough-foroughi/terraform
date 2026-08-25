4e36d0a1f7bd64b4d90e6f1f743b72c48f2354cd
forough-foroughi <foroughi.forough@gmail.com>
2026-08-25 19:05:39 +0200

    add elastic IP

--- aws/ec2/ec2.tf
+++ aws/ec2/ec2.tf
@@ -6,7 +6,25 @@ resource "aws_instance" "ec2-vm" {
     count         = var.ec2_instance_count
     user_data    = var.ec2_user_data
     security_groups = [aws_security_group.ec2-ssh.name, aws_security_group.ec2-web.name]
+    
     tags = {
         Name = "Terraform-EC2-Instance"
     }
     }
+
+resource "aws_eip" "ec2-eip" {
+    count = var.ec2_instance_count
+    domain = "vpc"
+
+    tags = {
+        Name = "Terraform-EC2-EIP"
+    }
+}
+
+resource "aws_eip_association" "ec2-eip-association" {
+    count         = var.ec2_instance_count
+
+    instance_id   = aws_instance.ec2-vm[count.index].id
+    allocation_id = aws_eip.ec2-eip[count.index].id
+}
+

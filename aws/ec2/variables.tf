92e57f4b364c3f426106c8e2424647dd3f3f61e8
forough-foroughi <foroughi.forough@gmail.com>
2026-09-01 16:35:53 +0200

    clean variable file

--- aws/ec2/variables.tf
+++ aws/ec2/variables.tf
@@ -2,61 +2,44 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "eu-central-1"
 }
 
 variable "allowed_ssh_cidr" {
   description = "CIDR block allowed for SSH access (e.g., your IP/32 or VPN CIDR)"
   type        = string
-  default     = "0.0.0.0/0" # WARNING: Change this to your IP for security!
 }
 
 variable "ec2_ami_id" {
   description = "AMI ID"
   type        = string
-  default     = "ami-0f417677f9bf398c1" # Amazon Linux AMI ID
 }
 
 variable "ec2_instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
 }
 
 variable "ec2_instance_count" {
   description = "Number of EC2 instances to create"
   type        = number
-  default     = 1
 }
 
 # key pair name for SSH access
 variable "ec2_key_name" {
   description = "Key pair name for SSH access"
   type        = string
-  default     = "terraform-key"
 }
 
 # user data script for EC2 instance
 variable "ec2_user_data" {
   description = "User data script for EC2 instance"
   type        = string
-  default     = <<-EOF
-                #!/bin/bash
-                # Use this for your user data (script from top to bottom)
-                # install httpd (Linux 2 version)
-                yum update -y
-                yum install -y httpd
-                systemctl start httpd
-                systemctl enable httpd
-                echo "<h1>Hello World from $(hostname -f) in AZ $EC2_AVAIL_ZONE</h1>" > /var/www/html/index.html
-                EOF
 }
 
 # placement group for EC2 instance
 variable "placement_group" {
   description = "Placement group name for EC2 instance"
   type        = string
-  default     = "placement-group-cluster"
 
   validation {
     condition = contains(
@@ -72,75 +55,55 @@ variable "placement_group" {
 variable "ec2_ebs_size" {
   description = "Size of the EBS volume in GB"
   type        = number
-  default     = 2
 }
 
 variable "ec2_ebs_type" {
   description = "Type of the EBS volume"
   type        = string
-  default     = "gp2"
 }
 
 variable "ec2_ebs_encrypted" {
   description = "Whether the EBS volume is encrypted"
   type        = bool
-  default     = false
 }
 
 variable "ec2_ebs_device_name" {
   description = "Device name for the EBS volume attachment"
   type        = string
-  default     = "/dev/sdf"
 }
 
 # EFS file system
 variable "ec2_efs_throughput_mode" {
   description = "Throughput mode for the EFS file system"
   type        = string
-  default     = "bursting" # Options: standard, bursting, provisioned
 }
 
 variable "ec2_efs_performance_mode" {
   description = "Performance mode for the EFS file system"
   type        = string
-  default     = "generalPurpose" # Options: generalPurpose, maxIO
 }
 
 # Application Load Balancer (ALB)
 variable "ec2_app_lb_type" {
   description = "Type of Application Load Balancer"
   type        = string
-  default     = "instance" # Options: instance, ip, lambda, alb
 }
 
 variable "ec2_app_lb_protocol_version" {
   description = "Protocol version for the Application Load Balancer"
   type        = string
-  default     = "HTTP1" # Options: HTTP1, HTTP2, GRPC
 }
 
 variable "route53_zone_name" {
   description = "Route 53 zone name"
   type        = string
-  default     = "app.forough-forough.com"
 }
 
 variable "route53_IP_address" {
   description = "Route 53 IP address"
   type        = string
-  default     = "192.168.1.100"
 }
 
 variable "weighted_route53_records" {
   description = "Route 53 IP address"
-  default = {
-    server1 = {
-      ip_address = "192.168.1.100"
-      weight     = 60
-    }
-    server2 = {
-      ip_address = "192.168.1.101"
-      weight     = 40
-    }
-  }
 }
